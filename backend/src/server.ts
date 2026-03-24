bf183d28aaebddf0413502aa6fb507278ba15092
LumiDesune <lumidesune@gmail.com>
2026-03-24 18:57:24 -0400

    feat: Adding middleware in server to handle errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,28 @@
-import Fastify from "fastify";
+import Fastify, { FastifyRequest, FastifyReply } from "fastify";
 import cors from "@fastify/cors";
 import { routes } from "./routes.js";
 
 // Criando a instância da aplicação
 const app = Fastify({ logger: true });
 
+// Registrando plugins da aplicação
+await app.register(cors);
+await app.register(routes);
+
+// Middleware para lidar com erros
+app.setErrorHandler((error: Error, request: FastifyRequest, reply: FastifyReply) => {
+    reply.status(500).send({ error: error.message || "Internal Server Error" });
+})
+
 // Iniciando o servidor
 const start = async () => {
 
-    await app.register(cors);
-    await app.register(routes);
 
     try {
-        await app.listen({port: 3333})
+        await app.listen({port: 3333, host: "0.0.0.0"})
+        console.log("Servidor rodando na porta 3333 => http://localhost:3333");
     } catch (error){
+        app.log.error(`Error ao iniciar o servidor: ${error}`);
         process.exit(1)
     }
 
